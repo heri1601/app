fcd984eeafe0e8b6a8cbe6bea5bd419a14ec71e7
user <heri1601>
2018-12-07 13:08:46 -0600

    Respaldo. Inserción correcta desde URL.

--- 02_NodeJS/Files/expressBodyParser.js
+++ 02_NodeJS/Files/expressBodyParser.js
@@ -216,16 +216,13 @@ class DAO {
 		console.log("registrando negocio");
 		
 		
-		
-		
-		
 		var MongoClient = require('mongodb').MongoClient;
 		var url = "mongodb://localhost:27017/";
 
 		MongoClient.connect(url, function(err, db) {
 		  if (err) throw err;
 		  var dbo = db.db("liverpool");
-		  var myobj = { name: "Company Inc", address: "Highway 37" };
+		  var myobj = { producto: inProducto, precio: inPrecio,imagen: inImagen };
 		  dbo.collection("productos").insertOne(myobj, function(err, res) {
 			if (err) throw err;
 			console.log("1 document inserted");
